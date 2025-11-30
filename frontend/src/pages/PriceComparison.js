db7c24b40a1e2f315d9a43e91b67ad24d7e23145
Patel2203 <patelboy1156@gmail.com>
2025-11-30 16:57:41 -0500

    api change

--- frontend/src/pages/PriceComparison.js
+++ frontend/src/pages/PriceComparison.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { useLocation } from 'react-router-dom';
-import axios from 'axios';
+import axios from '../api/axios';
 
 const PriceComparison = () => {
   const location = useLocation();
